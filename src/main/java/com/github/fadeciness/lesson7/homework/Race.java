2082e44dcd22831e5bf29e9b381a66fd9c10cdd7
fadeciness <solodimov.eugene@yandex.ru>
2023-03-15 10:51:15 +0300

    Added init homework 7

---
+++ src/main/java/com/github/fadeciness/lesson7/homework/Race.java
@@ -0,0 +1,12 @@
+package com.github.fadeciness.lesson7.homework;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Race {
+    private final ArrayList<Stage> stages;
+    public ArrayList<Stage> getStages() { return stages; }
+    public Race(Stage... stages) {
+        this.stages = new ArrayList<>(Arrays.asList(stages));
+    }
+}
